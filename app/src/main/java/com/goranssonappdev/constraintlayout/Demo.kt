d0a0d4bd4239c1571a36196b721c91ff3b386c57
goransson84 <goransson84@gmail.com>
2022-07-17 19:33:58 +0200

    C30. Working with ConstraintLayout in Compose - Adding constraints

--- app/src/main/java/com/goranssonappdev/constraintlayout/Demo.kt
+++ app/src/main/java/com/goranssonappdev/constraintlayout/Demo.kt
@@ -22,11 +22,29 @@ fun ConstraintLayoutDemo() {
 
         // Assigning a reference
         // Adding constraints
-        Text(text = "Hello" , modifier = Modifier.constrainAs(text1) {
-            // Constraints here
-            top.linkTo(parent.top, margin = 16.dp)
-            bottom.linkTo(parent.bottom, margin = 16.dp)
-        })
+//        Text(text = "Hello" , modifier = Modifier.constrainAs(text1) {
+//            // Constraints here
+//            top.linkTo(parent.top, margin = 16.dp)
+//            bottom.linkTo(parent.bottom, margin = 16.dp)
+//        })
+
+        // Multiple constraints
+//        Text(text = "Hello", modifier = Modifier.constrainAs(text1) {
+//            linkTo(parent.top, parent.bottom, bias = 0.8f)
+//            linkTo(button1.end, button2.start)
+//        })
+
+        // Centered horizontally and vertically
+//        Text(text = "text1", modifier = Modifier.constrainAs(text1) {
+//            centerVerticallyTo(text2)
+//            centerHorizontallyTo(parent)
+//        })
+
+        // centerAround()
+//        Text(text = "text1", modifier = Modifier.constrainAs(text1) {
+//            centerAround(text2.end)
+//            centerAround(text4.top)
+//        })
 
         // Multiple references
         val (button, text2, text3) = createRefs()
