91000e934d9bc919ad2ad0ed969472001298290a
goransson84 <goransson84@gmail.com>
2022-07-17 18:30:37 +0200

    C30. Working with ConstraintLayout in Compose - Calling ConstraintLayout

---
+++ app/src/main/java/com/goranssonappdev/constraintlayout/Demo.kt
@@ -0,0 +1,19 @@
+package com.goranssonappdev.constraintlayout
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+
+@Composable
+fun ConstraintLayoutDemo() {
+
+    ConstraintLayout(Modifier
+        .size(width = 200.dp, height = 300.dp)
+        .background(Color.Green)) {
+
+    }
+}
