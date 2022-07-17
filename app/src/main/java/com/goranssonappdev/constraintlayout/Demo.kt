f64b19c8bfdbc8f9c5ce73e3a7d05418a9a9d7f4
goransson84 <goransson84@gmail.com>
2022-07-17 18:47:31 +0200

    C30. Working with ConstraintLayout in Compose - Assigning a reference to a composable

--- app/src/main/java/com/goranssonappdev/constraintlayout/Demo.kt
+++ app/src/main/java/com/goranssonappdev/constraintlayout/Demo.kt
@@ -2,8 +2,10 @@ package com.goranssonappdev.constraintlayout
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -17,6 +19,11 @@ fun ConstraintLayoutDemo() {
 
         // Single reference
         val text1 = createRef()
+
+        // Assigning a reference
+        Text(text = "Hello" , modifier = Modifier.constrainAs(text1) {
+            // Constraints here
+        })
 
         // Multiple references
         val (button, text2, text3) = createRefs()
